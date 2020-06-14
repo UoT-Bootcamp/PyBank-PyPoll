86cf5004ee219003352eb7dac2e27572bdae47d6
Preeti Vishwakarma <preeti@Preetis-MacBook-Pro.local>
2020-06-14 13:38:50 -0400

    Calculate net total amount of Profit/Losses over the entire period

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,20 +1,47 @@
+#Import os and csv modules
 import os
 import csv
-# /Users/preeti/Desktop/GIT/Python---Challenge/PyBank/Resources
+
+#Define the path of budget_data.csv file
 csvpath = os.path.join("/Users/preeti/Desktop/GIT/Python---Challenge/PyBank/Resources", 'budget_data.csv')
 
+#Open the budget_data.csv file in read mode
 with open(csvpath, 'r') as csvfile:
+    
+    #Read the file using delimiter ","
     csvreader = csv.reader(csvfile, delimiter=",")
+    
+    #Read the header row first 
     csv_header = next(csvreader)
-    print(csv_header)
 
+    #declare an empty list for holding number of months, total profit & losses
     months_num = []
+    total_profit_losses = []
 
+    # Read each row of data after the header
     for row in csvreader:
+
+        #Append months in the empty list 'months_num'
         months_num.append(row[0])
-        # print(row)
-    number_of_months = len(months_num)
-    #print(number_of_months)
+        #Append profit/losses in the empty list 'total_profit_losses'
+        total_profit_losses.append(row[1])
+        #convert element of the list 'total_profit_losses' from string to integer
+        total_profit_losses = [int(num) for num in total_profit_losses]
+        #Calculate sum of all the elements in the list 'total_profit_losses'
+        sum_profit_losses = sum(total_profit_losses)
+    
+    #Print the final result
+    print("Financial Analysis")
+    print("------------------------------")
+    print(f"Total Months: {len(months_num)}")
+    print(f"Total: ${sum_profit_losses}")
+
+
+        
+        
+       
+    
+  
 
 
 
