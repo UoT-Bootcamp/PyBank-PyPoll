3014463755649aa22210cc18ac87b0d42d17ac8d
Preeti Vishwakarma <preeti@Preetis-MacBook-Pro.local>
2020-06-16 17:22:57 -0400

    Code cleanup

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,7 +9,6 @@ csvpath = os.path.join("/Users/preeti/Desktop/GIT/Python---Challenge/PyPoll/Reso
 total_number_votes = []
 candidate = {}
 percent_votes = {}
-winner = []
 
 #Open the csv file
 with open(csvpath) as csvfile:
@@ -21,61 +20,61 @@ with open(csvpath) as csvfile:
     #loop through the rows in the csv file
     for row in csv_reader:
         
-        #Append the rows in the empty list 'total_number votes'
+        #Append the rows in the empty list 'total_number votes' and calculate total numbver of votes
         total_number_votes.append(row)
+        total_votes = len(total_number_votes)
 
         #set the key in the dictionary
         key_name = row[2]
         
         #Check whether the key is not in the dictionary 'candidate', if it is then...
         if key_name not in candidate:
-            
             #set the value of the key to 0
             candidate[key_name]= 0
+        
         #increase the value of the key each time it encounter the key
         candidate[key_name] += 1
-        #calculate the total number of votes
-        total_votes = len(total_number_votes)
-        #calculate the percent of votes and round the number
+
+        #calculate the percentage of votes
         percent_calc = (candidate[key_name] / total_votes) * 100
-        ####percent_calc = format((candidate[key_name] / total_votes) * 100, '.3f')
-        #format the above calculation to 3 decimal places
         
         #assign the percent of votes of each candidates to their key
         percent_votes[key_name] = percent_calc
     
         #calculate the maximum of all votes
-        all_values = percent_votes.values()
-        max_value = max(all_values)
-
+        max_value = max(percent_votes.values())
+        
     #calculate the name of the candidate who got maximum percentage of vote 
     for name, percentage in percent_votes.items():
         if percentage == max_value:
             winner_name = name
 
-#print the final output   
+#write the output to a new .txt file
+pyPoll = open(os.path.join("/Users/preeti/Desktop/GIT/Python---Challenge/PyPoll/Analysis", "PyPoll.txt"), 'w')
+
+#Print the output in terminal
 print("Election Results\n")
 print("------------------------------\n")
 print("Total Votes: " + str(total_votes) + "\n")
 print("------------------------------\n")
 
+#write the output in .txt file
+pyPoll.write("Election Results\n")
+pyPoll.write("------------------------------\n")
+pyPoll.write("Total Votes: " + str(total_votes) + "\n")
+pyPoll.write("------------------------------\n")
 
+#sort the dictionary 'percent_votes' and iterate
 for new_name, percentage_of_votes in sorted(percent_votes.items(), key=lambda item:item[1], reverse =True):
-    
-    print(str(new_name) + ": " + format(percentage_of_votes, '.3f') + "% (" + str(candidate[new_name]) + ")" + "\n")   
+    print(str(new_name) + ": " + format(percentage_of_votes, '.3f') + "% (" + str(candidate[new_name]) + ")" + "\n")
+    pyPoll.write(str(new_name) + ": " + format(percentage_of_votes, '.3f') + "% (" + str(candidate[new_name]) + ")" + "\n")
+
+#Print the output in terminal
 print("------------------------------\n")
 print("Winner: " + winner_name + "\n")
 print("------------------------------")
 
-#write to a output to a new .txt file
-x = open(os.path.join("/Users/preeti/Desktop/GIT/Python---Challenge/PyPoll/Analysis", "PyPoll.txt"), 'w')
-
-x.write("Election Results\n")
-x.write("------------------------------\n")
-x.write("Total Votes: " + str(total_votes) + "\n")
-x.write("------------------------------\n")
-for new_name, percentage_of_votes in sorted(percent_votes.items(), key=lambda item:item[1], reverse =True):
-    x.write(str(new_name) + ": " + format(percentage_of_votes, '.3f') + "% (" + str(candidate[new_name]) + ")" + "\n")
-x.write("------------------------------\n")
-x.write("Winner: " + winner_name + "\n") 
-x.write("------------------------------") 
+#write the output in .txt file
+pyPoll.write("------------------------------\n")
+pyPoll.write("Winner: " + winner_name + "\n") 
+pyPoll.write("------------------------------") 
